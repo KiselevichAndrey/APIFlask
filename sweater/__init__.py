730d7832043182a67081ef54f761186697982da5
KiselevichAndrey <andkisand@gmail.com>
2020-05-06 12:42:51 +0300

    first commit

--- sweater/__init__.py
+++ sweater/__init__.py
@@ -5,11 +5,12 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = 'some secret salt'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123@localhost/py_sweater'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost/py_sweater'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 manager = LoginManager(app)
 
 from sweater import models, routes
 
+app.run()
 db.create_all()
